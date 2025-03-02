868a8fe7b4a5f95067f3da2df4e96302f2e69b26
Aysha Sahra <ayshasahra@gmail.com>
2025-03-02 20:05:38 +0530

    hidden card is navigated to the landing page

--- client/src/components/pages/FavouritesPage.js
+++ client/src/components/pages/FavouritesPage.js
@@ -218,7 +218,7 @@ export default function FavouritePage() {
                                     </p>
                                     <button 
                                     className='bg-white text-black font-Andika font-semibold opacity-0 transition-opacity duration-300 group-hover:opacity-100 content-center text-m w-1/2 rounded-full px-3 py-2 pt-1'
-                                    onClick={() => navigate(`/destination/${place._id}`)}
+                                    onClick={() => navigate(`/hidden-spots/${place._id}`)}
                                 >
                                     Explore more
                                 </button>
